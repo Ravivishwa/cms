ba78453351811bef3a5f302e3e542b187852da66
ravi <raviv.vishwakarma@gmail.com>
2020-01-24 00:57:06 +0530

    modified home page streucture

--- includes/header.php
+++ includes/header.php
@@ -14,3 +14,68 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+    <div class="container-fluid" style="padding: 0" id="navbar-header1 ">
+    <nav class="navbar navbar-expand-lg" id="navbar-1">
+      <a class="navbar-brand" id="navlink-1" href=""></a>
+        <ul class="navbar-nav ml-auto ul-list1">
+          <li class="dropdown nav1-list"><a class="dropdown nav-link nav-dropdown1" data-toggle="dropdown" href="#">
+            <span class="fas fa-ad icon-1"></span>MEGA PROMOTION<span class="fas fa-chevron-down icon-1"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Page 1-1</a></li>
+                <li><a href="#">Page 1-2</a></li>
+              </ul>
+          </li>
+          <li class="dropdown nav1-list">
+            <a class="dropdown nav-link nav-dropdown1"  href="login.php">
+            <span class="fas fa-user-secret icon-1"></span>Login/Register
+            <!-- <span class="fas fa-chevron-down icon-1"></span>  -->
+            </a>
+     <!--          <ul class="dropdown-menu">
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Page 1-2</a></li>
+              </ul> -->
+          </li>
+          <li class="dropdown nav1-list"><a class="dropdown nav-link nav-dropdown1" data-toggle="dropdown" href="#">
+            <span class="fas fa-shopping-cart icon-1"></span>RM 0.00</a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Page 1-1</a></li>
+                <li><a href="#">Page 1-2</a></li>
+              </ul>
+          </li>
+        </ul>
+    </nav>
+  </div>
+     <!--header navbar 2-->
+  <div class="container" id= "navbar-header2">
+    <nav class="navbar navbar-light navbar-expand-lg  navbar-2" >
+      <a class="navbar-brand" href=""><img class="logo1" src="img/logo1.png"></a>
+      <button class="navbar-toggler" id="toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto" >
+            <li class="nav-item item2">
+              <a class="nav-link" id="nav-link2" id="lid" href="#">Home</a>
+            <li class="nav-item item2">
+              <a class="nav-link" id="nav-link2" href="#">About Us</a>
+            </li>
+            <li class="nav-item dropdown item2">
+              <a class="nav-link" id="nav-link2" data-toggle="dropdown" href="#cta">Our Services
+               <span class="fas fa-chevron-down icon-1 fa-xs"></a>
+              <ul class="dropdown-menu">
+                <li><a href=""></a>  lemanweb dev</li>
+              </ul>
+            </li>
+            <li class="nav-item item2">
+              <a class="nav-link" id="nav-link2" href="#">eStore</a>
+            </li> 
+            <li class="nav-item item2">
+              <a class="nav-link" id="nav-link2" href="#">eNews</a>
+            </li>
+            <li class="nav-item item2">
+              <a class="nav-link" id="nav-link2" href="">Contact Us</a>
+            </li>
+          </ul>
+        </div>    
+    </nav>
+  </div>  
--- pages/home.php
+++ pages/home.php
@@ -1,167 +1,90 @@
-  <div class=" container-fluid" style="padding: 0" id="navbar-header1 ">
-    <nav class="navbar navbar-expand-lg" id="navbar-1">
-      <a class="navbar-brand" id="navlink-1" href=""></a>
-        <ul class="navbar-nav ml-auto ul-list1">
-          <li class="dropdown nav1-list"><a class="dropdown nav-link nav-dropdown1" data-toggle="dropdown" href="#">
-            <span class="fas fa-ad icon-1"></span>MEGA PROMOTION<span class="fas fa-chevron-down icon-1"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Page 1-1</a></li>
-                <li><a href="#">Page 1-2</a></li>
-              </ul>
-          </li>
-          <li class="dropdown nav1-list">
-            <a class="dropdown nav-link nav-dropdown1"  href="login.php">
-            <span class="fas fa-user-secret icon-1"></span>Login/Register
-            <!-- <span class="fas fa-chevron-down icon-1"></span>  -->
-            </a>
-     <!--          <ul class="dropdown-menu">
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Page 1-2</a></li>
-              </ul> -->
-          </li>
-          <li class="dropdown nav1-list"><a class="dropdown nav-link nav-dropdown1" data-toggle="dropdown" href="#">
-            <span class="fas fa-shopping-cart icon-1"></span>RM 0.00</a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Page 1-1</a></li>
-                <li><a href="#">Page 1-2</a></li>
-              </ul>
-          </li>
-        </ul>
-    </nav>
-  </div>
-     <!--header navbar 2-->
-  <div class="container" id= "navbar-header2">
-    <nav class="navbar navbar-light navbar-expand-lg  navbar-2" >
-      <a class="navbar-brand" href=""><img class="logo1" src="img/logo1.png"></a>
-      <button class="navbar-toggler" id="toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto" >
-            <li class="nav-item item2">
-              <a class="nav-link" id="nav-link2" id="lid" href="#">Home</a>
-            <li class="nav-item item2">
-              <a class="nav-link" id="nav-link2" href="#">About Us</a>
-            </li>
-            <li class="nav-item dropdown item2">
-              <a class="nav-link" id="nav-link2" data-toggle="dropdown" href="#cta">Our Services
-               <span class="fas fa-chevron-down icon-1 fa-xs"></a>
-              <ul class="dropdown-menu">
-                <li><a href=""></a>  lemanweb dev</li>
-              </ul>
-            </li>
-            <li class="nav-item item2">
-              <a class="nav-link" id="nav-link2" href="#">eStore</a>
-            </li> 
-            <li class="nav-item item2">
-              <a class="nav-link" id="nav-link2" href="#">eNews</a>
-            </li>
-            <li class="nav-item item2">
-              <a class="nav-link" id="nav-link2" href="">Contact Us</a>
-            </li>
-          </ul>
-        </div>    
-    </nav>
-  </div>  
-
-
 <div class="container-fluid">
   <div class="alert alert-info" role="alert">
   <strong style="display: inline-block;">ALERT/NOTICE :</strong><marquee style="width: 89%;float:right;display: inline-block;">This is new notice.</marquee>
   </div>
   <div class="container-fluid">
     <div class="row">
-    <!-- quick links -->
-    <div class="col-sm-9">
-      <div><h5><strong>QUICK LINKS</strong></h5></div>
-    
-      <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/home.png"></div>
-      <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/calendar.png"></div>
-      <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/sms.png"></div>
-      <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/search (1).png"></div>
-      <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/seo-and-web.png"></div>
-      <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/sms.png"></div>
-      <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/graphic.png"></div>
-      <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/team.png"></div>
-      <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/lock.png"></div>
-    </div>
-    <!-- announcements -->
-    <div class="col-sm-3">
-      <div class="card">
-        <div class="card-header">
-          Announcements
+      <div class="col-sm-9">
+        <div class="row">
+          <div class="col-sm-12">
+            <div><h5><strong>QUICK LINKS</strong></h5></div>    
+            <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/home.png"></div>
+            <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/calendar.png"></div>
+            <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/sms.png"></div>
+            <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/search (1).png"></div>
+            <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/seo-and-web.png"></div>
+            <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/sms.png"></div>
+            <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/graphic.png"></div>
+            <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/team.png"></div>
+            <div style="display:inline-block"><img style="width: 50px;height: 50px;display:inline-block" src="img/lock.png"></div>
+          </div>
         </div>
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          
-        </div>
-        <div class="card-footer text-muted">
-          2 days ago
-        </div>
-      </div>      
-  </div>
-</div>
-<br>
-  <div class="row">
-    <div class="col-sm-3">
-      <div class="card">
-        <div class="card-header">
-          Upcoming events
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      
-        </div>
-        <div class="card-footer text-muted">
-          2 days ago
-        </div>
-      </div>  
-    </div>
-    <div class="col-sm-6">
-      <h5>Maintenance Notice</h5><hr>
-      <p class="content-para"> Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
-      <p class="content-para">sed do eiusmod dolor sit amet, consectetur adipisicing</p>
-      <p class="content-para">tuptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <hr>
-      <h5>24 Hours Customer Service Help Desk</h5>
-      <p>Supplier</p>
-      <p><span class="fas fa-print"></span>   +7978456123</p>
-      <p><span  class="fas fa-phone-alt"></span>  +1268525013</p>
-      <p><span  class="fas fa-envelope"></span>  +1268525013</p>
-    </div>
-    <div class="col-sm-3">
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="card">
-            <div class="card-header">
-              Tag Cloud
+        <div class="row pt-4">
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="card-header">
+                Upcoming Events
+              </div>
+              <div class="card-body">
+                <div class="alert alert-warning" role="alert">
+                  <strong>there is no events!</strong>
+                </div>
+                <ul class="events-list">
+                  <li>Events 1</li><br>
+                  <li>Event 2</li><br>
+                  <li>Event 3</li><br>
+                  <li>Event 4</li><br>
+                  <li>Event 5</li><br>
+                </ul>                
+              </div>
             </div>
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-             
+          </div>
+          <div class="col-sm-8">
+            <h5>Maintenance Notice</h5><hr>
+            <p class="content-para"> Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+            <p class="content-para">sed do eiusmod dolor sit amet, consectetur adipisicing</p>
+            <p class="content-para">tuptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <hr>
+            <h5>24 Hours Customer Service Help Desk</h5>
+            <p>Supplier</p>
+            <p><span class="fas fa-print"></span>   +7978456123</p>
+            <p><span  class="fas fa-phone-alt"></span>  +1268525013</p>
+            <p><span  class="fas fa-envelope"></span>  +1268525013</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="card" >
+              <div class="card-body" id="rightside-card">
+                <h5 class="card-title">Announcements</h5>
+                <p class="card-contents">With supporting text below as a natural lead-in to additional content.  Lorem ipsum dolor sit amet, consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
             </div>
-            <div class="card-footer text-muted">
-              2 days ago
+          </div> 
+          </div>
+          <div class="col-sm-12 pt-2">
+            <div class="card">
+            <div class="card-body" id="rightside-card">
+              <h5 class="card-title">Tag cloud</h5>
+              <p class="card-contents">With supporting text below as a natural lead-in to additional content  Lorem ipsum dolor sit amet, consectetur adipisicing elit  Lorem ipsum dolor sit amet, consectetur adipisicing elit  Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </div>
           </div>            
-        </div>
-        <div class="col-sm-12">
-            <div class="card-header">
-              Polls
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              
-            </div>
-            <div class="card-footer text-muted">
-              2 days ago
-            </div>          
+          </div>          
+          <div class="col-sm-12 pt-2">
+            <div class="card">
+              <div class="card-body" id="rightside-card">
+                <h5>POLLS</h5>
+                <p class="card-contents" >How did you hear about us?</p>
+                <p class="card-contents"><input type="radio" name="card"> a. From friends, family members</p>
+                <p class="card-contents"><input type="radio" name="card"> b. From advertisements and     etc.</p>
+                <p class="card-contents"><input type="radio" name="card"> c. From another websites </p>
+                <p class="card-contents"><input type="radio" name="card"> d. Others</p>
+              </div>
+          </div>             
+          </div>
         </div>
       </div>
     </div>
